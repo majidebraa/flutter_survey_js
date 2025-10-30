09a06a0d0e96c31bfe881945bedfc9b1d13641e4
mjdebm@yahoo.com <Mm09131705971>
2025-10-30 18:24:59 +0330

    update pubspecs

--- packages/flutter_survey_js/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/flutter_survey_js/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import app_settings
 import file_selector_macos
 import flutter_js
 import shared_preferences_foundation
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterJsPlugin.register(with: registry.registrar(forPlugin: "FlutterJsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
